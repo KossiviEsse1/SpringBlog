9bc25a17c09c5f147d84bcdf5763fa2144cc569e
KossiviEsse1 <kossivi14@gmail.com>
2019-12-06 05:01:13 -0600

    most of my work

---
+++ src/test/java/org/koko/kokoprojectone/ProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koko.kokoprojectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
